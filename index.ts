yathomasi/permissions-policy

Allow any feature
Previously, there was an allowlist of features. If you tried to set
something outside of that list, an error would be thrown.

Now, this checking is not done and any key is allowed.

index.ts
@@ -14,54 +14,10 @@ function isQuoted(value: string) {
   return /^".*"$/.test(value);
 }
 
-function getHeaderValueFromOptions(options: unknown): string {
-  const FEATURES: Record<string, string> = {
-    accelerometer: "accelerometer",
-    ambientLightSensor: "ambient-light-sensor",
-    autoplay: "autoplay",
-    battery: "battery",
-    camera: "camera",
-    displayCapture: "display-capture",
-    documentDomain: "document-domain",
-    documentWrite: "document-write",
-    encryptedMedia: "encrypted-media",
-    executionWhileNotRendered: "execution-while-not-rendered",
-    executionWhileOutOfViewport: "execution-while-out-of-viewport",
-    fontDisplayLateSwap: "font-display-late-swap",
-    fullscreen: "fullscreen",
-    geolocation: "geolocation",
-    gyroscope: "gyroscope",
-    interestCohort: "interest-cohort",
-    layoutAnimations: "layout-animations",
-    legacyImageFormats: "legacy-image-formats",
-    loadingFrameDefaultEager: "loading-frame-default-eager",
-    magnetometer: "magnetometer",
-    microphone: "microphone",
-    midi: "midi",
-    navigationOverride: "navigation-override",
-    notifications: "notifications",
-    oversizedImages: "oversized-images",
-    payment: "payment",
-    pictureInPicture: "picture-in-picture",
-    publickeyCredentials: "publickey-credentials",
-    push: "push",
-    serial: "serial",
-    speaker: "speaker",
-    syncScript: "sync-script",
-    syncXhr: "sync-xhr",
-    unoptimizedImages: "unoptimized-images",
-    unoptimizedLosslessImages: "unoptimized-lossless-images",
-    unoptimizedLossyImages: "unoptimized-lossy-images",
-    unsizedMedia: "unsized-media",
-    usb: "usb",
-    verticalScroll: "vertical-scroll",
-    vibrate: "vibrate",
-    vr: "vr",
-    wakeLock: "wake-lock",
-    xr: "xr",
-    xrSpatialTracking: "xr-spatial-tracking",
-  };
+const dashify = (str: string): string =>
+  str.replace(/[A-Z]/g, (capitalLetter) => "-" + capitalLetter.toLowerCase());
 
+function getHeaderValueFromOptions(options: unknown): string {
   if (!isPlainObject(options)) {
     throw new Error(
       "permissionsPolicy must be called with an object argument. See the documentation."
@@ -77,14 +33,6 @@ function getHeaderValueFromOptions(options: unknown): string {
 
   const result = Object.entries(features)
     .map(([featureKeyCamelCase, featureValue]) => {
-      if (
-        !Object.prototype.hasOwnProperty.call(FEATURES, featureKeyCamelCase)
-      ) {
-        throw new Error(
-          `permissionsPolicy does not support the "${featureKeyCamelCase}" feature.`
-        );
-      }
-
       if (!Array.isArray(featureValue)) {
         throw new Error(
           `The value of the "${featureKeyCamelCase}" feature must be array of strings.`
@@ -130,7 +78,7 @@ function getHeaderValueFromOptions(options: unknown): string {
         }
       }
 
-      const featureKeyDashed = FEATURES[featureKeyCamelCase];
+      const featureKeyDashed = dashify(featureKeyCamelCase);
       const featureValuesUnion = featureValue.join(" ");
       return `${featureKeyDashed}=(${featureValuesUnion})`;
     })

helmetjs/feature-policy

index.ts
@@ -14,54 +14,10 @@ function isQuoted(value: string) {
   return /^".*"$/.test(value);
 }
 
-function getHeaderValue(options: unknown): string {
-  const FEATURES: Record<string, string> = {
-    accelerometer: "accelerometer",
-    ambientLightSensor: "ambient-light-sensor",
-    autoplay: "autoplay",
-    battery: "battery",
-    camera: "camera",
-    displayCapture: "display-capture",
-    documentDomain: "document-domain",
-    documentWrite: "document-write",
-    encryptedMedia: "encrypted-media",
-    executionWhileNotRendered: "execution-while-not-rendered",
-    executionWhileOutOfViewport: "execution-while-out-of-viewport",
-    fontDisplayLateSwap: "font-display-late-swap",
-    fullscreen: "fullscreen",
-    geolocation: "geolocation",
-    gyroscope: "gyroscope",
-    interestCohort: "interest-cohort",
-    layoutAnimations: "layout-animations",
-    legacyImageFormats: "legacy-image-formats",
-    loadingFrameDefaultEager: "loading-frame-default-eager",
-    magnetometer: "magnetometer",
-    microphone: "microphone",
-    midi: "midi",
-    navigationOverride: "navigation-override",
-    notifications: "notifications",
-    oversizedImages: "oversized-images",
-    payment: "payment",
-    pictureInPicture: "picture-in-picture",
-    publickeyCredentials: "publickey-credentials",
-    push: "push",
-    serial: "serial",
-    speaker: "speaker",
-    syncScript: "sync-script",
-    syncXhr: "sync-xhr",
-    unoptimizedImages: "unoptimized-images",
-    unoptimizedLosslessImages: "unoptimized-lossless-images",
-    unoptimizedLossyImages: "unoptimized-lossy-images",
-    unsizedMedia: "unsized-media",
-    usb: "usb",
-    verticalScroll: "vertical-scroll",
-    vibrate: "vibrate",
-    vr: "vr",
-    wakeLock: "wake-lock",
-    xr: "xr",
-    xrSpatialTracking: "xr-spatial-tracking",
-  };
+const dashify = (str: string): string =>
+  str.replace(/[A-Z]/g, (capitalLetter) => "-" + capitalLetter.toLowerCase());
 
+function getHeaderValue(options: unknown): string {
   if (!isPlainObject(options)) {
     throw new Error(
       "permissionsPolicy must be called with an object argument. See the documentation.",
@@ -77,14 +33,6 @@ function getHeaderValue(options: unknown): string {
 
   const result = Object.entries(features)
     .map(([featureKeyCamelCase, featureValue]) => {
-      if (
-        !Object.prototype.hasOwnProperty.call(FEATURES, featureKeyCamelCase)
-      ) {
-        throw new Error(
-          `permissionsPolicy does not support the "${featureKeyCamelCase}" feature.`,
-        );
-      }
-
       if (!Array.isArray(featureValue) || featureValue.length === 0) {
         throw new Error(
           `The value of the "${featureKeyCamelCase}" feature must be a non-empty array of strings.`,
@@ -129,7 +77,7 @@ function getHeaderValue(options: unknown): string {
         }
       }
 
-      const featureKeyDashed = FEATURES[featureKeyCamelCase];
+      const featureKeyDashed = dashify(featureKeyCamelCase);
       const featureValuesUnion = featureValue.join(" ");
       return `${featureKeyDashed}=(${featureValuesUnion})`;
     })